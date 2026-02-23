0783b2ddc7911e28384a6ddb947295e2403e10ca
artemtipa228@gmail.com <artemtipa228@gmail.com>
2026-02-23 20:39:52 +0200

    added history of orders

--- ReactApp2.Server/Models/DTO/Order.cs
+++ ReactApp2.Server/Models/DTO/Order.cs
@@ -6,4 +6,15 @@
         public List<int> ServiceIds { get; set; } // Список обраних послуг (зробимо списком на майбутнє, раптом клієнт захоче мийку + хімчистку одразу)
         public string? UserComments { get; set; } // Побажання клієнта
     }
+
+    public class OrderResponse
+    {
+        public int Id { get; set; }
+        public string CarInfo { get; set; } 
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; } 
+        public DateTime CreatedAt { get; set; }
+        public string? UserComments { get; set; }
+        public List<string> ServiceNames { get; set; } 
+    }
 }
